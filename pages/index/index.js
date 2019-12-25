b14a5752d27979cdd15f3089751fa3ea96d961e9
Terry Wang <lvterry@gmail.com>
2019-12-25 14:33:18 +0800

    news list page

---
+++ pages/index/index.js
@@ -0,0 +1,45 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const categoryCodes = ['gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other'];
+const util = require('../../utils/util.js');
+
+Page({
+  data: {
+    categories: ['国内', '国际', '财经', '娱乐', '军事', '体育', '其他'],
+    selectedIndex: 0
+  },
+
+  onLoad () {
+    this.selectedIndex = 0;
+    this.getNews()
+  },
+
+  onCategoryTapped (event) {
+    if (this.selectedIndex !== event.target.dataset.index) {
+      this.selectedIndex = event.target.dataset.index
+      this.setData({
+        selectedIndex: this.selectedIndex
+      })
+      this.getNews()
+    } 
+  },
+
+  getNews() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: categoryCodes[this.selectedIndex]
+      },
+      success: (res) => {
+        let result = res.data.result
+        result.forEach((item, key) => 
+          item.time = util.shortTime(new Date(item.date))
+        )
+        this.setData({
+          newsItems: result
+        })
+      }
+    })
+  }
+})
